f92d52e3a7e530dbb1f9da621673035cc94d7186
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-01-20 20:54:32 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            int time = 20;
+            int temporaryAge;
+            Console.WriteLine(age);
+            Console.WriteLine(time);
+            temporaryAge = age;
+            age = time;
+            time = temporaryAge;
+            Console.WriteLine(age);
+            Console.WriteLine(time);
+        }
+    }
+}
